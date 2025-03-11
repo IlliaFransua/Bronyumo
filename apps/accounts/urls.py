1bcfc99e80cccab0d37a6355410ae203af4c3e95
skxlpv <olexandertym0000@gmail.com>
2025-03-11 16:42:09 +0200

    perf: Delete unused urls

--- apps/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, enrepreneur_page, save_table_layout
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('entrepreneur-panel/', enrepreneur_page, name='enrepreneur_page'),
-    path('save-table-layout/', save_table_layout, name="save_table_layout")
-]
