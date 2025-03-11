97ba79dcd0b71ac4b40528cca8d89599255ef286
skxlpv <olexandertym0000@gmail.com>
2025-03-11 16:41:51 +0200

    perf: Update settings.py env configuration.

--- Bronyumo/settings.py
+++ Bronyumo/settings.py
@@ -25,7 +25,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
-load_dotenv(BASE_DIR / '.env.dist')
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
