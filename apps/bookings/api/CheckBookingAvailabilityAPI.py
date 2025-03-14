b51c06ae4d841f093a32bd383a165f985bf35253
IlliaFransua <illia.fransua@icloud.com>
2025-03-14 18:33:53 +0100

    Refactor: Changed path param name

--- apps/bookings/api/CheckBookingAvailabilityAPI.py
+++ apps/bookings/api/CheckBookingAvailabilityAPI.py
@@ -7,12 +7,12 @@ from rest_framework.response import Response
 
 class CheckBookingAvailabilityAPI(APIView):
     @staticmethod
-    def get(request, booking_hash):
+    def get(request, map_hash):
         # Check booking availability logic
         availability_data = {
             "available": True,
             "message": "Booking is available.",
-            "booking_hash": booking_hash,
+            "map_hash": map_hash,
         }
 
         return Response(availability_data)
