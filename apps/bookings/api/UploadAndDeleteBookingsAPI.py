ef6e24719010b42481a2475deac03f1b6d767ebc
vivdychenko.maxim2006@gmail.com <vivdychenko.maxim2006@gmail.com>
2025-03-16 22:59:34 +0200

    fixed UploadAndDeletebookingsAPI.py(added corect company_id scanning)

--- apps/bookings/api/UploadAndDeleteBookingsAPI.py
+++ apps/bookings/api/UploadAndDeleteBookingsAPI.py
@@ -1,12 +1,15 @@
 from django.utils.decorators import method_decorator
 from django.views import View
+from typing import Optional
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from apps.utils.decorators import session_required
+from rest_framework import status
+from rest_framework.views import APIView
 from apps.accounts.DatabaseConnection import DatabaseConnection
 from apps.accounts.managers import CompanyManager, CompanySessionManager
 from Bronyumo.settings import db_dsn
-from apps.main.models import Map, BookingObject, BookingRecord, Companies, CompanyMap, CompanySession
 
 
 # Example of model import
@@ -14,7 +17,7 @@ from apps.main.models import Map, BookingObject, BookingRecord, Companies, Compa
 
 
 @method_decorator(session_required, name='dispatch')
-class UploadAndDeleteBookingsAPI(View):
+class UploadAndDeleteBookingsAPI(APIView):
     def __init__(self, **kwargs: dict) -> None:
         """
         Initializes the SignInAPI class, setting up the necessary managers for handling company and session data.
@@ -22,13 +25,25 @@ class UploadAndDeleteBookingsAPI(View):
         :param kwargs: Additional arguments passed to the parent class.
         """
         super().__init__(**kwargs)
+        self.db_dsn = db_dsn
         self.company_manager = CompanyManager(db_dsn=db_dsn)
         self.session_manager = CompanySessionManager(db_dsn=db_dsn)
-    @staticmethod
     def delete(self, request, map_hash):
-        company_id = request.session.get('company_id')
-        if not company_id:
-            return JsonResponse({'error': 'Company ID not found in session.'}, status=400)
+        try:
+            session_id: Optional[str] = request.COOKIES.get('session_id')
+            company_data: Optional[dict] = self.company_manager.get_company_by_session_id(session_id)
+
+            if not company_data:
+                return JsonResponse({"error": "Company not found."}, status=status.HTTP_400_BAD_REQUEST)
+
+            company_id = company_data.get("id")
+        except ObjectDoesNotExist as e:
+            return JsonResponse({"error": "Company data not found."}, status=status.HTTP_404_NOT_FOUND)
+        except KeyError as e:
+            return JsonResponse({"error": f"Missing key: {str(e)}."}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({"error": "An unexpected error occurred."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
 
         try:
             with DatabaseConnection(self.db_dsn) as cursor:
